396f043c89374be73058f22d548edae40489e54b
Richard Fogh <ttrompeter@hotmail.com>
2022-07-28 19:03:04 -0700

    Initial Commit

---
+++ PickleballReferee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PickleballReferee
+//
+//  Created by Tom Trompeter on 7/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PickleballReferee/PickleballRefereeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickleballRefereeApp.swift
+//  PickleballReferee
+//
+//  Created by Tom Trompeter on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct PickleballRefereeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
